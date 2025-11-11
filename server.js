35ab85e097221c98b3d26275997f4e5be36ae4fd
sarcasticccccc <mrminmrmini@gmail.com>
2025-11-11 18:16:54 +0530

    changs

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const path = require('path')
 
 const port = 3000
 
-const server = http.createServer()
+const server = http.createServer((req,res)=>{
+    cont filePath = path.join(__dirname,req.url === '/' ? "index.html":"req.url")
+
+})
 
 server.listen(port,()=>{
     console.log(`Server is listening on port ${port }`);
