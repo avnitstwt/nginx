c690be3a441b08f59501b8650da66e70c5b0c2a5
sarcasticccccc <mrminmrmini@gmail.com>
2025-11-11 18:09:40 +0530

    changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const http = require('http')
-const fs = require(fs)
+const fs = require('fs')
 const path = require('path')
 
 const port = 3000
+
+const server = http.createServer()
+
+server.listen(port,()=>{
+    console.log(`Server is listening on port ${port }`);
+    
+})
